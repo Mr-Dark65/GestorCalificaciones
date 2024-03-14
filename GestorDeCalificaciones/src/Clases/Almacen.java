01506baaa61a22425a0b65071c6897e0c32246d8
Mr-Dark65 <steevenloor2412@gmail.com>
2024-03-14 17:56:08 -0500

    Implementacion de clase Almacen

--- GestorDeCalificaciones/src/Clases/Almacen.java
+++ GestorDeCalificaciones/src/Clases/Almacen.java
@@ -1,9 +1,26 @@
 package Clases;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author USER
  */
 public class Almacen {
+    
+    private static Almacen instance;
+
+    public ArrayList<Estudiante> estudiantes = new ArrayList<>();
+    
+
+    private Almacen() {
+    }
+
+    public static Almacen getInstance() {
+        if (instance == null) {
+            instance = new Almacen();
+        }
+        return instance;
+    }
     
 }
