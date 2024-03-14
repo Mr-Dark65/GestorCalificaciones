d31b1397c860a3e7785ed80b96d3fde974807d8f
PAUL-23VE <paulvelastegui2016@gmail.com>
2024-03-14 16:46:08 -0500

    Creacion Docentes

---
+++ GestorDeCalificaciones/src/Clases/Materia.java
@@ -0,0 +1,22 @@
+package Clases;
+import java.util.ArrayList;
+/**
+ *
+ * @author USER
+ */
+public class Materia {
+    private String nombre;
+
+    public Materia(String nombre) {
+        this.nombre = nombre;
+    }
+
+    // Getter y setter
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
