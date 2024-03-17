7ec7c1da50575b386799659b88e0987f378908c0
Aracelly126 <araguangasig95@gmail.com>
2024-03-17 12:04:27 -0500

    Realizado modulo Insertar calificacion

--- GestorDeCalificaciones/src/Clases/Controles.java
+++ GestorDeCalificaciones/src/Clases/Controles.java
@@ -87,4 +87,69 @@ public class Controles {
         return false;
     }
 
+    public boolean validarFecha(String fechaString) {
+        int dia, mes, anio;
+        String[] fechaArray = fechaString.split("/");
+        try {
+            if (!(fechaArray.length == 3)) {
+                return false;
+            }
+            dia = Integer.parseInt(fechaArray[0]);
+            mes = Integer.parseInt(fechaArray[1]);
+            anio = Integer.parseInt(fechaArray[2]);
+        } catch (NumberFormatException e) {
+            //Formato de fechaHoy inválido. Por favor, ingrese una fechaHoy en el formato dd/mm/yyyy o d/m/yyyy
+            return false;
+        }
+        if (anio < 2024) {
+            return false;
+        }
+        if (mes >= 1 && mes <= 12) {
+
+            // Validar fecha biciesta(febrero)
+            if ((anio % 4 == 0) && ((anio % 100 != 0) || (anio % 400 == 0))) {
+                if (mes == 2 && dia >= 30) {
+                    return false;
+                }
+            } else {
+                if (mes == 2 && dia >= 29) {
+                    return false;
+                }
+            }
+
+            // Validar mes con 31 días
+            if ((mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12) && dia >= 32) {
+                return false;
+            } // Validar mes con 30 días
+            else if ((mes == 4 || mes == 6 || mes == 9 || mes == 11) && dia >= 31) {
+                return false;
+            }
+        } else if (mes > 31) {
+            mes = 0;
+            return false;
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean validarNota(String nota) {
+        double notaVerificada;
+
+        try {
+            notaVerificada = Double.parseDouble(nota);
+            return notaVerificada <= 10.0;
+        } catch (Exception e) {
+            return false;
+        }
+
+    }
+
+    public void txtOnlyDoubles(java.awt.event.KeyEvent evt) {
+        char c = evt.getKeyChar();
+        if (!(Character.isDigit(c) || c == '.')) {
+            evt.consume();
+        }
+    }
+
 }
--- GestorDeCalificaciones/src/Vista/frmRegCalifi.java
+++ GestorDeCalificaciones/src/Vista/frmRegCalifi.java
@@ -1,19 +1,18 @@
 package Vista;
 
 import Clases.Almacen;
+import Clases.Controles;
 import Clases.Docente;
 import Clases.Estudiante;
 import Clases.Materia;
-import Utilidades.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class frmRegCalifi extends javax.swing.JFrame {
-
-    public frmRegCalifi() {
-    }
-
+    private String cedulaProfeActual;
+    private int posProfeAlmacen;
+    Controles control = new Controles();
     public frmRegCalifi(String cedula) {
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -46,30 +45,30 @@ public class frmRegCalifi extends javax.swing.JFrame {
         //</editor-fold>
         initComponents();
         this.setLocationRelativeTo(this);
-        cargarProfesor(cedula);
+        this.cedulaProfeActual = cedula;
+        cargarProfesor(this.cedulaProfeActual);
         cbMateriasDoce.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (cbMateriasDoce.getItemCount()>0) {
-                    String materiSelec=cbMateriasDoce.getSelectedItem().toString();
-                   ArrayList<Estudiante> estud= cargarEstudiantesPorMateria(materiSelec);
-                   //elimino a los estudiantes y es como que ingresara denuevo todo
-                   cbEstudiantes.removeAllItems();
-                   
-                   for(Estudiante estudiantesagg: estud){
-                       cbEstudiantes.addItem(estudiantesagg.getNombre()+" "+estudiantesagg.getApellido());
-                   }
+                if (cbMateriasDoce.getItemCount() > 0) {
+                    String materiSelec = cbMateriasDoce.getSelectedItem().toString();
+                    ArrayList<Estudiante> estud = cargarEstudiantesPorMateria(materiSelec);
+                    //elimino a los estudiantes y es como que ingresara denuevo todo
+                    cbEstudiantes.removeAllItems();
+
+                    for (Estudiante estudiantesagg : estud) {
+                        cbEstudiantes.addItem(estudiantesagg.getNombre() + " " + estudiantesagg.getApellido());
+                    }
                 }
             }
-         });
+
+        });
 
     }
 
-
-
     private void cbMateriasDoceActionPerformed(ActionEvent evt) {
         String materiaSeleccionada = cbMateriasDoce.getSelectedItem().toString();
-          cargarEstudiantesPorMateria(materiaSeleccionada);
+        cargarEstudiantesPorMateria(materiaSeleccionada);
     }
 
     @SuppressWarnings("unchecked")
@@ -91,10 +90,10 @@ public class frmRegCalifi extends javax.swing.JFrame {
         jL2 = new javax.swing.JLabel();
         jL1 = new javax.swing.JLabel();
         jL = new javax.swing.JLabel();
-        txNota = new javax.swing.JTextField();
+        txtNota = new javax.swing.JTextField();
         btAceptarAggCal = new javax.swing.JButton();
         cbMateriasDoce = new javax.swing.JComboBox<>();
-        jTextField1 = new javax.swing.JTextField();
+        txtFecha = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("LibHib | Iniciar Sesion");
@@ -172,6 +171,11 @@ public class frmRegCalifi extends javax.swing.JFrame {
         cbEstudiantes.setBackground(new java.awt.Color(255, 255, 255));
         cbEstudiantes.setForeground(new java.awt.Color(0, 0, 0));
         cbEstudiantes.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(176, 16, 37)));
+        cbEstudiantes.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                cbEstudiantesMouseClicked(evt);
+            }
+        });
         jPanel1.add(cbEstudiantes, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 192, 160, 32));
 
         jL2.setForeground(new java.awt.Color(0, 0, 0));
@@ -186,26 +190,36 @@ public class frmRegCalifi extends javax.swing.JFrame {
         jL.setText("Estudiante:");
         jPanel1.add(jL, new org.netbeans.lib.awtextra.AbsoluteConstraints(64, 198, 80, -1));
 
-        txNota.setBackground(new java.awt.Color(255, 255, 255));
-        txNota.setForeground(new java.awt.Color(0, 0, 0));
-        txNota.setHorizontalAlignment(javax.swing.JTextField.CENTER);
-        txNota.setToolTipText("");
-        txNota.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(176, 16, 37)));
-        jPanel1.add(txNota, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 250, 96, 32));
+        txtNota.setBackground(new java.awt.Color(255, 255, 255));
+        txtNota.setForeground(new java.awt.Color(0, 0, 0));
+        txtNota.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        txtNota.setToolTipText("");
+        txtNota.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(176, 16, 37)));
+        txtNota.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtNotaActionPerformed(evt);
+            }
+        });
+        jPanel1.add(txtNota, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 250, 96, 32));
 
         btAceptarAggCal.setBackground(new java.awt.Color(176, 16, 37));
         btAceptarAggCal.setForeground(new java.awt.Color(255, 255, 255));
         btAceptarAggCal.setText("Aceptar");
+        btAceptarAggCal.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btAceptarAggCalActionPerformed(evt);
+            }
+        });
         jPanel1.add(btAceptarAggCal, new org.netbeans.lib.awtextra.AbsoluteConstraints(224, 352, 96, 32));
 
         cbMateriasDoce.setBackground(new java.awt.Color(255, 255, 255));
         cbMateriasDoce.setForeground(new java.awt.Color(0, 0, 0));
         jPanel1.add(cbMateriasDoce, new org.netbeans.lib.awtextra.AbsoluteConstraints(144, 96, 160, -1));
 
-        jTextField1.setBackground(new java.awt.Color(255, 255, 255));
-        jTextField1.setForeground(new java.awt.Color(0, 0, 0));
-        jTextField1.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(176, 16, 37)));
-        jPanel1.add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 300, 96, 32));
+        txtFecha.setBackground(new java.awt.Color(255, 255, 255));
+        txtFecha.setForeground(new java.awt.Color(0, 0, 0));
+        txtFecha.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(176, 16, 37)));
+        jPanel1.add(txtFecha, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 300, 96, 32));
 
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 576, 480));
 
@@ -222,6 +236,62 @@ public class frmRegCalifi extends javax.swing.JFrame {
         this.dispose();
     }//GEN-LAST:event_jlRegresarMouseClicked
 
+    private void txtNotaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtNotaActionPerformed
+        String nombreEstudiante = cbEstudiantes.getSelectedItem().toString();
+
+        double nota;
+        try {
+            nota = Double.parseDouble(txtNota.getText());
+        } catch (NumberFormatException e) {
+            System.out.println("Error: La nota ingresada no es válida.");
+            return;
+        }
+        ArrayList<Estudiante> estudiantes = cargarEstudiantesPorMateria(cbMateriasDoce.getSelectedItem().toString());
+
+        Estudiante estudianteSeleccionado = null;
+        for (Estudiante estudiante : estudiantes) {
+            if ((estudiante.getNombre() + " " + estudiante.getApellido()).equals(nombreEstudiante)) {
+                estudianteSeleccionado = estudiante;
+                break;
+            }
+        }
+        if (estudianteSeleccionado != null) {
+            estudianteSeleccionado.setNota(nota);
+
+            System.out.println("Nota ingresada correctamente para " + estudianteSeleccionado.getNombre() + " " + estudianteSeleccionado.getApellido() + ": " + nota);
+        } else {
+            System.out.println("Error: No se encontró al estudiante seleccionado.");
+        }
+    }//GEN-LAST:event_txtNotaActionPerformed
+
+    private void btAceptarAggCalActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btAceptarAggCalActionPerformed
+
+        if (!control.validarNota(txtNota.getText())) {
+            return;
+        }
+        if (!control.validarFecha(txtFecha.getText())) {
+            return;
+        }
+        Almacen.getInstance().informacion.get(this.posProfeAlmacen).
+                getMaterias().get(cbMateriasDoce.getSelectedIndex()).
+                getEstudiante().get(cbEstudiantes.getSelectedIndex()).
+                setNota(Double.parseDouble(txtNota.getText()));
+        System.out.println("Notita");
+        Almacen.getInstance().informacion.get(this.posProfeAlmacen).
+                getMaterias().get(cbMateriasDoce.getSelectedIndex()).
+                getEstudiante().get(cbEstudiantes.getSelectedIndex()).
+                setFecha(txtFecha.getText());
+        System.out.println("fwchita");
+
+        System.out.println(Almacen.getInstance().informacion.get(this.posProfeAlmacen).
+                getMaterias().get(cbMateriasDoce.getSelectedIndex()).
+                getEstudiante().get(cbEstudiantes.getSelectedIndex()).toString());
+    }//GEN-LAST:event_btAceptarAggCalActionPerformed
+
+    private void cbEstudiantesMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_cbEstudiantesMouseClicked
+        System.out.println(cbEstudiantes.getSelectedIndex());
+    }//GEN-LAST:event_cbEstudiantesMouseClicked
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel BarraSuperior;
     private javax.swing.JLabel IconCalificacion;
@@ -236,17 +306,18 @@ public class frmRegCalifi extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollBar jScrollBar1;
     private javax.swing.JSeparator jSeparator1;
-    private javax.swing.JTextField jTextField1;
     private javax.swing.JLabel jlRegresar;
     private javax.swing.JLabel lbCalificacion1;
     private javax.swing.JLabel lbCalificacion2;
     private javax.swing.JLabel lbNombre;
-    private javax.swing.JTextField txNota;
+    private javax.swing.JTextField txtFecha;
+    private javax.swing.JTextField txtNota;
     // End of variables declaration//GEN-END:variables
 
     private void cargarProfesor(String cedula) {
         for (int i = 0; i < Almacen.getInstance().informacion.size(); i++) {
             if (Almacen.getInstance().informacion.get(i).getCedula().equals(cedula)) {
+                this.posProfeAlmacen = i;
                 this.lbNombre.setText(Almacen.getInstance().informacion.get(i).getNombre() + " "
                         + Almacen.getInstance().informacion.get(i).getApellido());
                 ArrayList<Materia> materias = Almacen.getInstance().informacion.get(i).getMaterias();
@@ -267,16 +338,38 @@ public class frmRegCalifi extends javax.swing.JFrame {
 //            }
 //        }
 //    }
-
+    /*
     private ArrayList<Estudiante> cargarEstudiantesPorMateria(String materiaSeleccionada) {
-        for(Docente doce: Almacen.getInstance().informacion){
-           for(Materia mate: doce.getMaterias()){
-               if (mate.getNombre().equals(materiaSeleccionada)) {
+        for (Docente doce : Almacen.getInstance().informacion) {
+            for (Materia mate : doce.getMaterias()) {
+                if (mate.getNombre().equals(materiaSeleccionada)) {
                     ArrayList<Estudiante> estud = mate.getEstudiante();
                     return estud;
-               }
-           }
+                }
+            }
         }
         return null;
+    }*/
+    private ArrayList<Estudiante> cargarEstudiantesPorMateria(String materiaSeleccionada) {
+        ArrayList<Estudiante> estudiantes = new ArrayList<>();
+
+        for (Docente doce : Almacen.getInstance().informacion) {
+            for (Materia mate : doce.getMaterias()) {
+                if (mate.getNombre().equals(materiaSeleccionada)) {
+                    estudiantes = mate.getEstudiante();
+                    break;
+                }
+            }
+        }
+        cbEstudiantes.removeAllItems();
+
+        for (Estudiante estudiante : estudiantes) {
+            cbEstudiantes.addItem(estudiante.getNombre() + " " + estudiante.getApellido());
+        }
+
+        return estudiantes;
     }
+
+
+
 }
